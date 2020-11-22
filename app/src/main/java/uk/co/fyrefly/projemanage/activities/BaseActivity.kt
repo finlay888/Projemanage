3f711ced88c0fc54afe5daaf3fb49184ff1aecfe
finlay888 <finlayjmm@gmail.com>
2020-11-22 01:28:56 +0000

    commit

--- app/src/main/java/uk/co/fyrefly/projemanage/activities/BaseActivity.kt
+++ app/src/main/java/uk/co/fyrefly/projemanage/activities/BaseActivity.kt
@@ -19,7 +19,6 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
     }
 
     fun showProgressDialog(text: String) {
